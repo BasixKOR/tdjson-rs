d4181a28d1035ce2ea938955340c786a327590a5
Basix <basix@basix.tech>
2020-01-05 20:05:27 +0900

    Add OutOfRangeError on set_log_verbosity_level

--- src/log.rs
+++ src/log.rs
@@ -8,24 +8,25 @@ use std::fmt;
 
 /// This enum specifies
 #[derive(Debug)]
-pub enum LogFileError {
+pub enum LogError {
   TDLibError,
+  OutOfRangeError,
   CStringError(std::ffi::NulError),
 }
 
-impl fmt::Display for LogFileError {
+impl fmt::Display for LogError {
   fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-    if let LogFileError::CStringError(err) = self {
-      err.fmt(f)
-    } else {
-      write!(f, "TDLib error")
+    match self {
+      LogError::CStringError(err) => err.fmt(f),
+      LogError::TDLibError => write!(f, "TDLib error"),
+      LogError::OutOfRangeError => write!(f, "log_verbosity must be between 1 and 1024.")
     }
   }
 }
 
-impl Error for LogFileError {
+impl Error for LogError {
   fn source(&self) -> Option<&(dyn Error + 'static)> {
-    if let LogFileError::CStringError(err) = self {
+    if let LogError::CStringError(err) = self {
       Some(err)
     } else {
       None
@@ -33,26 +34,29 @@ impl Error for LogFileError {
   }
 }
 
-impl From<std::ffi::NulError> for LogFileError {
+impl From<std::ffi::NulError> for LogError {
   fn from(error: std::ffi::NulError) -> Self {
-    LogFileError::CStringError(error)
+    LogError::CStringError(error)
   }
 }
 
 /// Sets TDLib log file path for your application.`
-pub fn set_log_file(path: &str) -> Result<(), LogFileError> {
+pub fn set_log_file(path: &str) -> Result<(), LogError> {
   let cpath = CString::new(path)?;
   unsafe {
     if td_set_log_file_path(cpath.as_ptr()) == 1 {
       Ok(())
     } else {
-      Err(LogFileError::TDLibError)
+      Err(LogError::TDLibError)
     }
   }
 }
 
-pub fn set_log_verbosity_level(level: i32) {
-  unsafe {
-    td_set_log_verbosity_level(level);
+pub fn set_log_verbosity_level(level: i32) -> Result<(), LogError> {
+  if level < 1 || level > 1024 {
+    Err(LogError::OutOfRangeError)
+  } else {
+    unsafe { td_set_log_verbosity_level(level); };
+    Ok(())
   }
 }
