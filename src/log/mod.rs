5348916f67c6532cbf4c834b4564a1291b6a3dfe
Basix <basix@basix.tech>
2020-01-05 20:52:34 +0900

    Seperate LogError to another module
    The structure tree of module is same as last commit.

--- src/log/mod.rs
+++ src/log/mod.rs
@@ -2,46 +2,10 @@ extern crate tdjson_sys;
 
 use tdjson_sys::{td_set_log_file_path, td_set_log_verbosity_level};
 
-use std::error::Error;
 use std::ffi::CString;
-use std::fmt;
 
-/// Error when failed to perform log-related task.
-#[derive(Debug)]
-pub enum LogError {
-  /// TDLib failes to set a new log file.
-  TDLibError,
-  /// verbosity level is not between 1 and 1024.
-  OutOfRangeError,
-  /// Thrown if the log file path contains a zero byte.
-  CStringError(std::ffi::NulError),
-} // TODO Refactor this
-
-impl fmt::Display for LogError {
-  fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-    match self {
-      LogError::CStringError(err) => err.fmt(f),
-      LogError::TDLibError => write!(f, "TDLib failed to set a new log file."),
-      LogError::OutOfRangeError => write!(f, "log_verbosity must be between 1 and 1024."),
-    }
-  }
-}
-
-impl Error for LogError {
-  fn source(&self) -> Option<&(dyn Error + 'static)> {
-    if let LogError::CStringError(err) = self {
-      Some(err)
-    } else {
-      None
-    }
-  }
-}
-
-impl From<std::ffi::NulError> for LogError {
-  fn from(error: std::ffi::NulError) -> Self {
-    LogError::CStringError(error)
-  }
-}
+mod errors;
+pub use self::errors::*;
 
 /// Sets TDLib log file path for your application.
 ///
